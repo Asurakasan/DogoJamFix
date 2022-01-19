51be1e8de8d9bd5d1d0084d6640470ab4ad6f23d
Asurakasan <constantin.harchy@lacatholille.fr>
2022-01-19 17:19:59 +0100

    Update Player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private GameObject player;
-    /*[Header("Attack Point")]
+    [Header("Attack Point")]
     public Transform Punch1;
+    public float Punch1Range;
     public Transform Punch2;
     public Transform UpperCut;
     public Transform LowKick;
-    */
+    public float LowKickRange;
+    
     public Transform attackPoint;
+    public Transform ult;
 
     [Header("Attack Settings")]
     public float attackRange = 0.5f;
+    public float ultiRange = 5f;
     public LayerMask enemyLayers;
+
+    [Header("Ulti Stats")]
+    public int chargeUlt;
+    public int addPoint;
+    public int maxUlt;
+
+    [Header("Life Stats")]
+    public int currentLife;
+    public int maxLife;
 
     [Header("Movement Settings")]
     public float speed;
+    private float saveSpeed;
     public float jumpforce;
 
-
-    [Header("State")]
-    public bool IdleState;
-    public bool Walking;
-    public bool Jumping;
-    public bool Crouching;
 
     //[Header("Animation")]
     //public Animator animPlayer;
@@ -37,6 +45,9 @@ public class Player : MonoBehaviour
     {
         Rigid = GetComponent<Rigidbody2D>();
         player = this.gameObject;
+        currentLife = maxLife;
+        chargeUlt = 0;
+        saveSpeed = speed;
     }
     // Start is called before the first frame update
     void Update()
@@ -49,17 +60,23 @@ public class Player : MonoBehaviour
             Jump();
         if (Input.GetKeyDown(KeyCode.S))
             Crouch();
-
+        if (Input.GetKeyDown(KeyCode.R) && chargeUlt > maxUlt)
+            Ulti();
         if(!Input.anyKey)
             Idle();
+
+        if (Input.GetKeyUp(KeyCode.S))
+            transform.localScale = new Vector3(-1, 1, 1);
     }
 
     void Attack()
     {
+        speed = 0;
         Collider2D[] hitenemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         foreach (Collider2D enemy in hitenemies)
         {
             EnemyDamage(enemy);
+            chargeUlt += addPoint;
         }
     }
     void EnemyDamage(Collider2D enemy)
@@ -69,10 +86,12 @@ public class Player : MonoBehaviour
     void Idle()
     {
         //animPlayer.SetBool("walking", false);
-        //attackPoint = Punch1;
+        attackPoint = Punch1;
+        attackRange = Punch1Range;
     }
     void Walk()
     {
+        speed = saveSpeed;
         float horizontal = Input.GetAxis("Horizontal")*speed;
         horizontal *= Time.deltaTime;
 
@@ -84,22 +103,36 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
 
 
-        //attackPoint = Punch1;
+        attackPoint = Punch1;
     }
     void Jump()
     {
         Rigid.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
-        //attackPoint = UpperCut;
+        attackPoint = UpperCut;
     }
     void Crouch()
     {
-        //attackPoint = LowKick;
+        transform.localScale = new Vector3(-1, 0.7f, 1);
+        attackPoint = LowKick;
+        attackRange = LowKickRange;
+        
     }
-
+    void Ulti()
+    {
+        Collider2D[] ultenemies = Physics2D.OverlapCircleAll(ult.position, ultiRange, enemyLayers);
+        foreach (Collider2D enemy in ultenemies)
+        {
+            EnemyDamage(enemy);
+        }
+        chargeUlt = 0;
+    }
     private void OnDrawGizmosSelected()
     {
         if (attackPoint == null)
             return;
+        if (ult == null)
+            return;
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        Gizmos.DrawWireSphere(ult.position, ultiRange);
     }
 }
