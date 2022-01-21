d2a22d4ec28b22fda6ce35479175ae36df33aa04
P3D\ngruszecki <nicolas.gruszecki@lacatholille.fr>
2022-01-21 10:36:07 +0100

    Pour voir si ça push bien

--- Assets/ScriptEnnemi/EnemyCat.cs
+++ Assets/ScriptEnnemi/EnemyCat.cs
@@ -2,27 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ennemyone : Enemy
+public class EnemyCat : EnemyBase2
 {
-    // Start is called before the first frame update
+
+    public float DetectionRange2;
+    public int jumpforce;
+
+     
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+        
     }
 
     // Update is called once per frame
     protected override void Update()
     {
+       
         base.Update();
+
+
+
+
+
+
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision) //Changement de sens
     {
-
-        right = !right;
-
+        //RigidCat.AddForce(new Vector2(10, jumpforce), ForceMode2D.Impulse);
+        //right = !right;
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("chat");
+            //RigidCat.AddForce(new Vector2(10, jumpforce), ForceMode2D.Impulse);
+        }
     }
 
     protected override void PlayerDamage(Collider2D player)
@@ -30,4 +47,9 @@ public class ennemyone : Enemy
         base.PlayerDamage(player);
 
     }
+
+
+
+
+
 }
