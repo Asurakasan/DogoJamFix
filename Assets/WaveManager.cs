4aaba727a9b0fd69b9edeee70557eab1edcedecd
toml-c <tom.le-corvic@lacatholille.fr>
2022-01-30 20:19:17 +0100

    not fixed yet

--- Assets/WaveManager.cs
+++ Assets/WaveManager.cs
@@ -7,47 +7,52 @@ public class WaveManager : MonoBehaviour
     [SerializeField]
     private List<EnemyManager> ennemyManager;
     private EnemyManager currentEnemy;
-     GameObject[] ennemyObject;
+     
     bool isaded = false;
     private MainGame mainGame;
+    public bool canadd = false;
 
     // Start is called before the first frame update
     void Start()
     {
+
         mainGame = MainGame.instance;
         
     }
     public IEnumerator WaveSpawn()
     {
-       
+
         for (int h = 0; h < ennemyManager.Count;)
         {
-            currentEnemy = ennemyManager[h];
-            mainGame.countdown = currentEnemy.CoolDownBeforeSpawn;
-            yield return new WaitForSeconds(currentEnemy.CoolDownBeforeSpawn);
-            for (int j = 0; j < currentEnemy.EnemyCount; j++)
-            {
-                if(currentEnemy == null)
-                {
-                    mainGame.isEnter = false;
-                    mainGame.ArenaWall[mainGame.index].SetActive(false);
+
+
+                    currentEnemy = ennemyManager[h];
+                    mainGame.countdown = currentEnemy.CoolDownBeforeSpawn;
+                    yield return new WaitForSeconds(currentEnemy.CoolDownBeforeSpawn);
+                    for (int j = 0; j < currentEnemy.EnemyCount; j++)
+                    {
+
+                        if (currentEnemy == null)
+                        {
+                            mainGame.isEnter = false;
+                            mainGame.ArenaWall[mainGame.index].SetActive(false);
+                        }
+                        else
+                        {
+                            mainGame.SpawnEnemy(currentEnemy.Enemy, currentEnemy.spawnPoint);
+                            mainGame.ennemyObject = GameObject.FindGameObjectsWithTag("Ennemis");
+                            isaded = true;
+                            yield return new WaitForSeconds(0.5f);
+
+                        }
+
+               
+
+                    //currentEnemy.EnemyCount++;
+                    h++;
+
+
                 }
-                else
-                {
-                    mainGame.SpawnEnemy(currentEnemy.Enemy, currentEnemy.spawnPoint);
-                    ennemyObject = GameObject.FindGameObjectsWithTag("Ennemis");
-                    isaded = true;
-                    yield return new WaitForSeconds(0.5f);
-                  
-                }
-
-            }
-
-            //currentEnemy.EnemyCount++;
-            h++;
-
-
-
         }
 
 
@@ -55,19 +60,25 @@ public class WaveManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isaded == true)
-        {
-            foreach (var item in ennemyObject)
+            if (isaded == true)
             {
-                if (item == null)
+                foreach (var item in mainGame.ennemyObject)
                 {
-                    mainGame.ArenaWall[mainGame.index].SetActive(false);
-                    isaded = false;
+                    if (item == null)
+                    {
+                        mainGame.ArenaWall[mainGame.index].SetActive(false);
+                        mainGame.TriggerArene[mainGame.index].GetComponent<Collider2D>().enabled = !mainGame.TriggerArene[mainGame.index].GetComponent<Collider2D>().enabled;
+                        isaded = false;
+                    }
 
                 }
+
+
+
             }
         }
+     
        
 
     }
-}
+
