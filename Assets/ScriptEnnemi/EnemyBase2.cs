769428652ef3aa2ca11a9805e22044075cc496e6
DESKTOP-A1LFEKH\PC <nicolas.gruszecki@lacatholille.fr>
2022-02-01 00:16:00 +0100

    Réquilibrage des wave et changement player

--- Assets/ScriptEnnemi/EnemyBase2.cs
+++ Assets/ScriptEnnemi/EnemyBase2.cs
@@ -79,19 +79,21 @@ public class EnemyBase2 : MonoBehaviour
         {
             //Destroy(gameObject);
             
+
             mainGame.ennemylist.Remove(gameObject);
+            Speed = 0;
+            DetectionRange = 0;
             //mainGame.Canexplore(); J'ai commenter cette ligne car elle fesait buger le jeu et je ne comprenais pas pourquoi
             if (Player.GetComponent<Player>().Droite)
             {
                 gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-                Speed =0;
+               
                 rb.AddForce(new Vector2(150,5));
 
             }
             else if(Player.GetComponent<Player>().Gauche)
             {
                 gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
-                Speed = 0;
                 rb.AddForce(new Vector2(-150,5));
 
             }
